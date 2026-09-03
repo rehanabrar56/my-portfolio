dff1befd5c6625cb092653e4371fa46e54381064
rehanabrar56 <rehanabrar56@gmail.com>
2026-09-03 13:24:19 +0500

    Delete src/main/java/com/rehan/portfolio/repository directory

--- src/main/java/com/rehan/portfolio/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rehan.portfolio.repository;
-
-import com.rehan.portfolio.entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
